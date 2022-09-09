8b0313631ac9fa50569cdb8d2c99d6074c7202c2
ismail adebiyi <ismailadebiyi@gmail.com>
2022-09-08 22:00:17 -0700

    add_5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - function to return a string
+ * Return: 0 as output
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,");
+return (0);
+}
