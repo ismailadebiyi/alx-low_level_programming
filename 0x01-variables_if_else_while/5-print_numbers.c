8b5c75d02c1a7437142761b61ab19cc055c4c217
ismail adebiyi <ismailadebiyi@gmail.com>
2022-09-11 07:02:51 -0700

    add_5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - function that list 0-9.
+ * Return: 0
+ */
+int main(void)
+{
+int n;
+for (n = 0; n < 10; n++)
+{
+print("%d",n);
+}
+print("\n");
+return (0);
+}
