aa602c82035ce535c4f00c6a77ad6f6ec999e121
ismail adebiyi <ismailadebiyi@gmail.com>
2022-09-11 14:48:24 -0700

    add_101

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -7,11 +7,14 @@ int main(void)
 {
 int i;
 int j;
+int k;
 for (i = 0; i < 10; i++)
 {
 for (j = 0; j < 10; j++)
 {
-if (i >= j)
+for (k = 0; k < 10; k++)
+{
+if ((i >= j) && (j >= k))
 {
 ;
 }
@@ -19,7 +22,8 @@ else
 {
 putchar(i + 48);
 putchar(j + 48);
-if ((i == 8) && (j == 9))
+putchar(k + 48);
+if ((i == 7) && (j == 8) && (k == 9))
 {
 ;
 }
