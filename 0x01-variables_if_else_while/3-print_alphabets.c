04c4ef79a55bd028ef3444610881b99e8464fd99
ismail adebiyi <ismailadebiyi@gmail.com>
2022-09-11 04:37:33 -0700

    add_3

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -2,17 +2,23 @@
 #include <stdlib.h>
 #include <time.h>
 /**
- * main - function list all the alphabelt in small letter.
+ * main - function list all the alphabelt in  both small and capital letter.
  * Return: 0
  */
 int main(void)
 {
 int n = 97;
+int i = 65;
 do {
 putchar(n);
 n++;
 }
 while (n < 123);
+do {
+putchar(i);
+i++;
+}
+while (i < 91);
 putchar(10);
 return (0);
 }
