0b998f3104393ca80d9fd53cde83c76293118b88
ismail adebiyi <ismailadebiyi@gmail.com>
2022-09-13 08:04:32 -0700

    add_1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * print_alphabet - function to output all alphabet in small letters
+ */
+
+void print_alphabet(void)
+{
+char c;
+for (c = "a"; c <= "z"; ++c)
+{
+_putchar(c);
+}
+}
