d003481a024e88c57f93391fda0291e6d680083d
ismail adebiyi <ismailadebiyi@gmail.com>
2022-09-14 02:17:45 -0700

    add_9

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <math.h>
+/**
+ * times_table - compute multiplication table of 0-9
+ */
+void times_table(void)
+{
+	int i, j;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			int s = (i * j);
+			_putchar(s);
+			if (j == 9)
+			{
+				;
+			}
+			_putchar(',');
+			_putchar(' ');
+			if (s > 10)
+			{
+				;
+			}
+			_putchar(' ');
+		}
+		_putchar('\n');
+	}
+}
