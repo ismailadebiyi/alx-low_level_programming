b167abafdc61d133fb979df988e6ccd725f29e67
ismail adebiyi <ismailadebiyi@gmail.com>
2022-09-14 01:12:24 -0700

    fix_11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -11,7 +11,7 @@ void print_to_98(int n)
 
 		for (i = n; i <= 98; i++)
 		{
-			_putchar("i");
+			_putchar(i + '0');
 			if (i == 98)
 			{
 				;
@@ -26,7 +26,7 @@ void print_to_98(int n)
 
 		for (i = n; i >= 98; i--)
 		{
-			_putchar("i");
+			_putchar(i + '0');
 			if (i == 98)
 			{
 				;
