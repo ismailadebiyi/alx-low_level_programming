1a4f93a4f6267b9709be8dc0df5f22cd3b7279a9
ismail adebiyi <ismailadebiyi@gmail.com>
2022-09-15 07:57:06 -0700

    add_1

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -6,7 +6,6 @@
 int main(void)
 {
 	int i;
-	
 	printf("Infinite loop incoming :(\n");
 	/**
 	 * i = 0;
@@ -18,3 +17,4 @@ int main(void)
 	printf("Infinite loop avoided! \\o/\n");
 	return (0);
 }
+
