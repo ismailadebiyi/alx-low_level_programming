2a60f29103b0d9ff663797324379c1482fea11e8
ismail adebiyi <ismailadebiyi@gmail.com>
2022-09-14 19:03:23 -0700

    add_102

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - print out the first 50 fib number
+ * Return: 0 always true
+ */
+int main(void)
+{
+	int f = 1, s = 2, n = 0, i;
+
+	printf("%d, %d, ", f, s);
+	for ( i = 2; i <= 50; i++)
+	{
+		n = f + s;
+		printf("%d, ", n);
+		f = s;
+		s = n;
+	}
+	printf('\n');
+	return (0);
+}
+
