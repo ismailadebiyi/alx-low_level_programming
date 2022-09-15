c280afabffcb923bf2b8ac65a0ab24ac82311e6a
ismail adebiyi <ismailadebiyi@gmail.com>
2022-09-15 09:48:53 -0700

    add_2

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -3,7 +3,15 @@
 #include <stdio.h>
 /**
  * positive_or_negative - check for state of number.
+ * @i: integer to check
+ *
+ * largest_number - check fo the largest number
+ * @a: first integer
+ * @b: second integer
+ * @c: third integer
+ *
  */
 void positive_or_negative(int i);
 
+int largest_number(int a, int b, int c)
 #endif /* MAIN_H */
