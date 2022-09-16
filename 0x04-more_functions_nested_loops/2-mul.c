94f180a16e02fdd02a426b447a13b40a94d5edf9
ismail adebiyi <ismailadebiyi@gmail.com>
2022-09-15 21:53:24 -0700

    add_2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include <math.h>
+#include "main.h"
+/**
+ * mul - multiply two integers
+ * @a: first Number
+ * @b: second number
+ * Return: product of a and b
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -3,6 +3,7 @@
  * _putchar - writes the character c to stdout
  * _isupper - check is the alphabet is in upper case
  * _isdigit- check for digit
+ * mul - multiply two integers
  * print_sign - print signed number
  * _abs - compute the absolute value of an integal
  * print_last_digit - compute the absolute last digit of a no
@@ -14,3 +15,4 @@ int _isupper(int c);
 
 int _isdigit(int c);
 
+int mul(int a, int b);
