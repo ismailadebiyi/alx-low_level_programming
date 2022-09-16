18e6b48ae84c1cf27c56dc8f4780bb4200a4c291
ismail adebiyi <ismailadebiyi@gmail.com>
2022-09-15 21:45:08 -0700

    add_1

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <ctype.h>
+/**
+ * _isdigit - method to check if a given paramater is a digit
+ * @c: parameter to check.
+ * Return: 1 if true or 0 if false
+ */
+int _isupper(int c)
+{
+	if (isdigit(c))
+	{
+		return (1);
+	}
+		return (0);
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -2,19 +2,15 @@
 /**
  * _putchar - writes the character c to stdout
  * _isupper - check is the alphabet is in upper case
- * _isalpha - check for alphabet
+ * _isdigit- check for digit
  * print_sign - print signed number
  * _abs - compute the absolute value of an integal
  * print_last_digit - compute the absolute last digit of a no
- * add - add two integers
- * print_to_98 - count up or down to 98
- * times_table - set times table from 0 -9
- * jack_bauer - count timer from 00:00 to 23:59
- * print_times_table - compute times table from 0 - 15
+ * 
  */
 int _putchar(char c);
 
 int _isupper(int c);
 
-int _isalpha(int c);
+int _isdigit(int c);
 
