173f3e24370d0dd65e9960be2ef77847bc6111d4
ismail adebiyi <ismailadebiyi@gmail.com>
2022-09-15 21:36:41 -0700

    add_0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <ctype.h>
+/**
+ * _isupper - method to check if a given letter is in upper case
+ * @c: letter to check.
+ * Return: 1 if true or 0 if false
+ */
+int _isupper(int c)
+{
+	if (isupper(c))
+	{
+		return (1);
+	}
+		return (0);
+}
