34d1b46ade7a74c42f05a44f02ff2ec93852d90b
ismail adebiyi <ismailadebiyi@gmail.com>
2022-09-16 09:34:12 -0700

    fix_5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -4,15 +4,18 @@
  */
 void more_numbers(void)
 {
-	int i;
+	int i, j;
 
-	for (i = 0; i <= 14; i++)
+	for (i = 0; i <= 10; i++)
 	{
-		if (i > 9)
+		for (j = 0; j <= 14; j++)
 		{
-			_putchar((i / 10) + '0');
+			if (i > 9)
+			{
+				_putchar((i / 10) + '0');
+			}
+		_putchar((i % 10) + '0');
 		}
-	_putchar((i % 10) + '0');
 	}
 	_putchar('\n');
 }
