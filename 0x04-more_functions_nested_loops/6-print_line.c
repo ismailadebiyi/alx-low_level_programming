17fb07a8328026127c5af0e688fdaa7cf9b5d7f3
ismail adebiyi <ismailadebiyi@gmail.com>
2022-09-16 11:03:03 -0700

    add_6

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,21 @@
+#include "main"
+/**
+ * print_line - function that print _ n times
+ * @n: Number of times to print _
+ */
+void print_line(int n)
+{
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		int i, j;
+
+		for (i = 0; i <= n; i++)
+		{
+			_putchar('_');
+		}
+	}
+}
