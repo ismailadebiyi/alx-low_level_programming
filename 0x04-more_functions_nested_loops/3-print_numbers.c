a5a0c47a06ce417f490a5c7ad6aafbd9777fcfc1
ismail adebiyi <ismailadebiyi@gmail.com>
2022-09-15 22:02:58 -0700

    add_3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_numbers - print the last digit of a given number
+ * 
+ */
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		_putchar(i + '0');
+	}
+}
+
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -4,7 +4,7 @@
  * _isupper - check is the alphabet is in upper case
  * _isdigit- check for digit
  * mul - multiply two integers
- * print_sign - print signed number
+ * print_numbers - print number from 0-9
  * _abs - compute the absolute value of an integal
  * print_last_digit - compute the absolute last digit of a no
  * 
@@ -16,3 +16,7 @@ int _isupper(int c);
 int _isdigit(int c);
 
 int mul(int a, int b);
+
+void print_numbers(void);
+
+
