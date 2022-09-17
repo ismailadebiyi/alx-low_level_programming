afc114fb1b93d7129514155abe1d4a89390b1e8b
ismail adebiyi <ismailadebiyi@gmail.com>
2022-09-16 22:17:42 -0700

    fix_Buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -8,21 +8,25 @@ int main(void)
 
 	for (i = 1; i <= 100; i++)
 	{
+		if (i > 1)
+		{
+			printf(" ");
+		}
 		if ((i % 3) == 0)
 		{
-			printf("Fizz ");
+			printf("Fizz");
 		}
 		else if ((i % 5) == 0)
 		{
-			printf("Buzz ");
+			printf("Buzz");
 		}
 		else if ((i % 3 == 0) && (i % 5 ==0))
 		{
-			printf("FizzBuzz ");
+			printf("FizzBuzz");
 		}
 		else
 		{
-			printf("%d ", i);
+			printf("%d", i);
 		}
 	}
 	printf("\n");
