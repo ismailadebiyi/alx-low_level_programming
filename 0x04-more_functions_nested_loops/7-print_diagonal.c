944fe9d9809f19ebebd05db47655af287116c1c8
ismail adebiyi <ismailadebiyi@gmail.com>
2022-09-17 08:56:39 -0700

    add_7

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -5,7 +5,7 @@
  */ 
 void print_diagonal(int n)
 {
-	if (n < = 0)
+	if (n <= 0)
 	{
 		_putchar('\n');
 	}
@@ -13,8 +13,18 @@ void print_diagonal(int n)
 	{
 		int i, j;
 
-		for (i = 1; i <= n; i++)
+		for (i = 0; i < n; i++)
 		{
-			for (j = 0; j < i; j++)
+			if ( i > 0)
 			{
+				for (j = 0; j < i; j++)
+				{
+					_putchar(' ');
+				}
+			}
+		_putchar('\\');
+		_putchar('\n');
+		}
+	}
+}
 
