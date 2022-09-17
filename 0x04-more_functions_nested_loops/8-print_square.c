a0fa35c2a25672c093fcb61b7ac075ccbf1aafc3
ismail adebiyi <ismailadebiyi@gmail.com>
2022-09-16 21:35:15 -0700

    add_8

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -5,13 +5,20 @@
  */
 void print_square(int n)
 {
-	int i, j;
-
-	for (i = 0; i < n; i++)
+	if (n <= 0)
 	{
-		for (j = 0; j <= n; j++)
+		_putchar('\n');
+	}
+	else
+	{
+		int i, j;
+		
+		for (i = 0; i < n; i++)
 		{
-			_putchar('#');
+			for (j = 0; j <= n; j++)
+			{
+				_putchar('#');
+			}
 		}
 		_putchar('\n');
 	}
