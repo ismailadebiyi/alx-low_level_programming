dab264e6f70886f92f9fbdc8900c44c9d775abe2
ismail adebiyi <ismailadebiyi@gmail.com>
2022-09-16 21:31:10 -0700

    add_8

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_diagonal - print \ n times
+ * @n: the number to print \
+ */ 
+void print_diagonal(int n)
+{
+	if (n < = 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		int i, j;
+
+		for (i = 1; i <= n; i++)
+		{
+			for (j = 0; j < i; j++)
+			{
+
---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_square - print # n by n times
+ * @n: number of time to print #
+ */
+void print_square(int n)
+{
+	int i, j;
+
+	for (i = 0; i < n; i++)
+	{
+		for (j = 0; j <= n; j++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+	}
+}
+
