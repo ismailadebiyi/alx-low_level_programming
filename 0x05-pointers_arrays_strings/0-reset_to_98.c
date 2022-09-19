45e84f7ccb27d283d648e989fe49d35a6b7ce899
ismail adebiyi <ismailadebiyi@gmail.com>
2022-09-19 13:35:07 -0700

    add_0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * reset_to_98 - reset a pointer to 98
+ * @*n: any given number
+ */
+void rest_to_98(int *n)
+{
+	*n = 98;
+}
+
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,34 @@
+/**
+ * _putchar - print single char
+ * reset_to_98 - reset the pointer n to 98
+ * swap_int - swap the integer pointer a and b
+ * _strlen - get the lenght of a string in pointer s
+ * _puts - print a string to stdout
+ * print_rev - print into stdout the reverse string of input
+ * rev_string - print a reverse of a string
+ * puts2 - print every other character
+ * print_array - print element of an array
+ * _strcpy - string copy
+ *
+ */
+
+void reset_to_98(int *n);
+
+void swap_int(int *a, int *b);
+
+int _strlen(char *s);
+
+void _puts(char *str);
+
+void print_rev(char *s);
+
+void rev_string(char *s);
+
+void put2(char *str);
+
+void puts_half(char *str);
+
+void print_array(int *a, int n);
+
+char *_strcpy(char *dest, char *src);
+
