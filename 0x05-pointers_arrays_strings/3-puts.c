f67b52cc1d83a34969148358663b179724613cab
ismail adebiyi <ismailadebiyi@gmail.com>
2022-09-19 21:54:42 -0700

    fix_3

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -7,11 +7,12 @@
  */
 void _puts(char *str)
 {
-	int i;
+	int i = 0;
 
-	for (i = 0; i < *(str + i); i++)
+	while (*(str + i) != '\0')
 	{
-		putchar(*(str + i));
+		_putchar(*(str + i));
+		i++;
 	}
 	_putchar('\n');
 }
