3bf4071f89dd1cb01e08a30336312f6ba3f590e7
ismail adebiyi <ismailadebiyi@gmail.com>
2022-09-20 05:02:09 -0700

    add_5

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -8,13 +8,16 @@ void rev_string(char *s)
 {
 	int i = 0, v = 0;
 
-	while (s[i] != '\0')
+	while (*(s + i) != '\0')
 	{
 		i++;
+		_putchar(i + '0');
 	}
 	while (i--)
 	{
-		s[i] = *(s + v);
+		_putchar(i + '0');
+		*(s + v) = s[i];
 		v++;
+		_putchar(v + '0')
 	}
 }
