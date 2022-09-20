57d8293f9b228ac80bb9e8c8221b9e9255730fa4
ismail adebiyi <ismailadebiyi@gmail.com>
2022-09-19 22:12:06 -0700

    add_4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _puts - print a string to stdout
+ * @str: string
+ *
+ */
+void _puts(char *str)
+{
+	int i = 0, v = 0;
+
+	while (*(str + i) != '\0')
+	{
+		v++;
+	}
+	while (v--)
+	{
+		_putchar(*(str + v));
+	}
+	_putchar('\n');
+}
