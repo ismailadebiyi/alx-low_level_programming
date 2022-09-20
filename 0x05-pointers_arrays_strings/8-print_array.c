e16d50f7c76ac0d8fac81e9b82d0753013015a5c
ismail adebiyi <ismailadebiyi@gmail.com>
2022-09-20 15:34:41 -0700

    add_8

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_array - print array from pointer
+ * @a: array pointer
+ * @n: array index
+ */
+void print_array(int *a, int n)
+{
+	int i = 0;
+
+	while (i < n)
+	{
+		if (i < (n -1))
+		{
+			printf("%d, ", a[i]);
+		}
+		else
+		{
+			printf("%d\n", a[i]);
+		}
+	i++;
+	}
+
+}
