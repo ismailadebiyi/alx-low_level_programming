ed7d0233ae69024e9bfcdd16703e161768e2a9b6
ismail adebiyi <ismailadebiyi@gmail.com>
2022-09-20 17:20:14 -0700

    add_9

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _strcpy - copy string from crc to destination
+ * @dest: destination
+ * @src: source
+ * return: value of the destination pointer
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	for (i = 0; src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
+	for (; i < n; i++)
+	{
+		dest[i] = '\0';
+	}
+	return dest;
+}
