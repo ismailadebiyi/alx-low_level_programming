c7fd08844079b0d1c0046f238e23c107ba00b720
ismail adebiyi <ismailadebiyi@gmail.com>
2022-09-22 11:28:33 -0700

    add_5

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * string_toupper - change a lower case string to upper case
+ * @s: character
+ * Return: character
+ */
+char *string_toupper(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		if (s[i] >= 'a' && s[i] <= 'z')
+		{
+			s[i] -= 32;
+		}
+		i++;
+	}
+	return (s);
+}
