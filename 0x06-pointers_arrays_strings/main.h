bd5718a34265443b30a54491d0151ad2320ec1f5
ismail adebiyi <ismailadebiyi@gmail.com>
2022-09-22 02:47:25 -0700

    add_0

---
+++ 0x06-pointers_arrays_strings/main.h
@@ -0,0 +1,30 @@
+/**
+ * _putchar - print single char
+ * _strcat - append 2 strings into destination
+ * _strncat - append n byte character 
+ * _strncpy - copy n byte charter to destination
+ * _strcmp - compare 2 strings
+ * reverse_array - print array from back
+ * string_toupper - change string to uppercase
+ * cap_string - capitalize all the dtring in a sentence
+ * leet - encode a string
+ *
+ */
+int _putchar(char c);
+
+char *_strcat(char *dest, char *src);
+
+char *_strncat(char *dest, char *src, int n);
+
+char *_strncpy(char *dest, char *src, int n);
+
+int _strcmp(char *s1, char *s2);
+
+void reverse_array(int *a, int n);
+
+char *string_toupper(char *);
+
+char *cap_string(char *);
+
+char *leet(char *);
+
