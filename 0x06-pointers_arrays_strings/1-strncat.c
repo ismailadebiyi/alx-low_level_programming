f83e02ab94417a41f24111f5c764ee9fe93bd4c5
ismail adebiyi <ismailadebiyi@gmail.com>
2022-09-22 03:21:28 -0700

    add_1

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -6,7 +6,7 @@
  * @n: No of source bit to copy
  * Return: dest as the default pointer
  */
-char *_strcat(char *dest, char *src, int n)
+char *_strncat(char *dest, char *src, int n)
 {
 	int i = 0, d = 0;
 
