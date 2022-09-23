f0f0871951af73abb58be537ef5213d36c438eb2
ismail adebiyi <ismailadebiyi@gmail.com>
2022-09-22 21:27:37 -0700

    add_6

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,39 @@
+#include "main.h"
+/**
+ * cap_string - capitalize all words of a string
+ * @s: character
+ * Return: character
+ */
+char *cap_string(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		if (!(s[i] >= 'a') && (s[i] <= 'z'))
+		{
+			;
+		}
+		else
+		{
+			if (s[i - 1] == ' ' ||
+					s[i - 1] == '\t' ||
+					s[i - 1] == '\n' ||
+					s[i - 1] == ',' ||
+					s[i - 1] == ';' ||
+					s[i - 1] == '.' ||
+					s[i - 1] == '!' ||
+					s[i - 1] == '?' ||
+					s[i - 1] == '"' ||
+					s[i - 1] == '(' ||
+					s[i - 1] == ')' ||
+					s[i - 1] == '{' ||
+					s[i - 1] == '}')
+			{
+				s[i] -= 32;
+			}
+		}
+		i++;
+	}
+	return (s);
+}
