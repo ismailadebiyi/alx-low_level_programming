6d69c49e0c564f333facc05b26283acc0b10399c
ismail adebiyi <ismailadebiyi@gmail.com>
2022-09-27 21:56:29 -0700

    add_4

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * _strpbrk - serch string
+ * @s: string to check
+ * @accept: check against
+ * Return: pointer
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	unsigned int i = 0, j = 0;
+
+	while (s[i] != '\0')
+	{
+		while (accept[j] != '\0')
+		{
+			if (s[i] == accept[j])
+			{
+				break;
+			}
+		}
+		if (accept[j])
+		{
+			return (s + i);
+		}
+	}
+	return (0);
+}
