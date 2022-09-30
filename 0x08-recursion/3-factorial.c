0349dbb4d94a5c82a7da7cf5c1e7c5d87b514f9e
ismail adebiyi <ismailadebiyi@gmail.com>
2022-09-29 19:14:08 -0700

    add_all

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * factorial - find the factorial of a number n
+ * @n: factorial of number
+ * Return: the factorial of the number
+ */
+int factorial(int n)
+{
+    if (n < 0)
+    {
+        return (-1);
+    }
+    if (n == 0)
+    {
+        return (1);
+    }
+    else
+    {
+        return (n * factorial(n - 1));
+    }
+}
---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _pow_recursion - find the power of a number
+ * @x: Number
+ * @y: Power
+ * Return: Answer
+ */
+int _pow_recursion(int x, int y)
+{
+    if (y < 0)
+    {
+        return (-1);
+    }
+    if (y == 0)
+    {
+        return (1);
+    }
+    else
+    {
+        return (x * _pow_recursion(x, y-1));
+    }
+}
