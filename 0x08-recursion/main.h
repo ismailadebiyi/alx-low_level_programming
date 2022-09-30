d22e22e2aa4405775112835705a1119ff6f0cbfa
ismail adebiyi <ismailadebiyi@gmail.com>
2022-09-29 20:13:22 -0700

    fix_e

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -14,6 +14,6 @@ int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
 int _sqrt(int n, int x)
 int is_prime_number(int n);
-int is _prime(int n, int start);
+int is_prime(int n, int start);
 
 #endif /*MAIN_H*/
