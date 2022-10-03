f6f1836c7820cc57336ede49523bd38b54f5b85f
ismail adebiyi <ismailadebiyi@gmail.com>
2022-10-03 08:38:57 -0700

    add_4

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,40 +1,32 @@
-#include "main.h"
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <ctype.h>
+
 /**
- * main - find the sum of argurment
- * @argc: agurgment count
- * @argv: argument array
- * Return: 0 if true, 1 if false
+ * main - adds numbers
+ * @argc: number of arguments passed to the function
+ * @argv: argument vector of pointers to strings
+ *
+ * Return: 0 if no errors, else 1
  */
 int main(int argc, char *argv[])
 {
-	int i, j, s = 0;
+	int a = 0, i, j;
 
-	if (argc > 1)
+	for (i = 1; i < argc; i++)
 	{
-		for (i = 1; i < argc; i++)
+		for (j = 0; argv[i][j]; j++)
 		{
-			for (j = 0; argv[i][j]; j++)
+			if (isdigit(argv[i][j]) == 0)
 			{
-				if (isdigit(argv[i][j]) == 0)
-				{
-					printf("Error\n");
-					return (1);
-				}
+				puts("Error");
+				return (1);
 			}
 		}
-		for (i = 1; i < argc; i++)
-		{
-			s += atoi(argv[i]);
-		}
-		printf("%d\n", s);
 	}
-	else
+	for (i = 1; i < argc; i++)
 	{
-		printf("Error\n");
-		return (1);
+		a += atoi(argv[i]);
 	}
+	printf("%d\n", a);
 	return (0);
-}
