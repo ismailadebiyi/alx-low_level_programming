109d879024873eae814727568870f5e0f6c9fd1a
ismail adebiyi <ismailadebiyi@gmail.com>
2022-10-03 08:23:11 -0700

    add_4

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,39 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - find the sum of argurment
+ * @argc: agurgment count
+ * @argv: argument array
+ * Return: 0 if true, 1 if false
+ */
+int main(int argc, char *argv[])
+{
+	int i, j, s = 0;
+
+	if (argc > 2)
+	{
+		for (i = 1; i < argc; i++)
+		{
+			for (j = 0; argv[i][j]; j++)
+			{
+				if (isdigit(argv[i][j] == 0))
+				{
+					printf("Error\n");
+					return (1);
+				}
+			}
+		}
+		for (i = 1; i < argc; i++)
+		{
+			s += atoi(argv[i]);
+		}
+		printf("%d\n", p);
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
+	return (0);
+}
