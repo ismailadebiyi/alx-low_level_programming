9a46446f9f5bcdfc6e94e88ab6ccfc197e7ac383
ismail adebiyi <ismailadebiyi@gmail.com>
2022-10-04 20:59:13 -0700

    add_0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -13,17 +13,13 @@ char *create_array(unsigned int size, char c)
 	char *s;
 
 	s = malloc(sizeof(char) * size);
-	if (size != 0 || s != NULL)
-	{
-		for (i = 0; i != size; i++)
-		{
-			s[i] = c;
-		}
-		return (s);
-	}
-	else
+	if (size == 0 || s == NULL)
 	{
 		return (NULL);
 	}
-	return (NULL);
+	for (i = 0; i != size; i++)
+	{
+		s[i] = c;
+	}
+	return (s);
 }
