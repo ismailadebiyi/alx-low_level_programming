aa0798f7780b1a090b43698a9187b9dfa23d69a0
ismail adebiyi <ismailadebiyi@gmail.com>
2022-10-04 21:14:13 -0700

    add_1

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,35 @@
+#include "main.h"
+/**
+ * _strdup -returns a pointerof a newly allocated
+ * string
+ * @str: given string
+ * Return: NULL if Error, else Pointer to allocated space
+ */
+char *_strdup(char *str)
+{
+	char *cp;
+	int i, len = 0;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; str[i]; i++)
+	{
+		len++;
+	}
+	cp = malloc(sizeof(char) * (len + 1));
+
+	if (cp == NULL)
+	{
+		return (NULL);
+	}
+	for (index = 0; str[i]; i++)
+	{
+		cp[i] = str[i];
+	}
+	cp[len] = '\0';
+
+	return (cp);
+}
+
