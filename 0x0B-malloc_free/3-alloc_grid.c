2bcf9fa4ce433a0168c5333c857c8e701818ba54
ismail adebiyi <ismailadebiyi@gmail.com>
2022-10-04 22:31:19 -0700

    add_3

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -29,19 +29,19 @@ int **alloc_grid(int width, int height)
 				{
 					a[i][j] = 0;
 				}
-				else
-				{
-					while (i >= 0)
-					{
-						free(a[i]);
-						i++;
-					}
-					free(a);
-					return (NULL);
-				}
 			}
-			return (a);
+			else
+			{
+				while (i >= 0)
+				{
+					free(a[i]);
+					i++;
+				}
+				free(a);
+				return (NULL);
+			}
 		}
+		return (a);
 	}
 	else
 	{
