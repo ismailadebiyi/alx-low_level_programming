2a48697f25b090e3051edba6053b0d949bd58142
ismail adebiyi <ismailadebiyi@gmail.com>
2022-10-04 20:47:50 -0700

    add_0

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdlib.h>
+#include <limits.h>
+/**
+ * create_array - fill the first n bytes of memory pointed to by s with b
+ * @c: constant to be filled with
+ * @size: number of byte to be filled
+ * Return: pointer s
+ */
+char *create_array(unsigned int size, char c)
+{
+	unsigned int i;
+	char *s;
+
+	s = malloc(sizeof(char) * size);
+	if (size != 0)
+	{
+		for (i = 0; i != size; i++)
+		{
+			s[i] = c;
+		}
+		return (s);
+	}
+	else
+	{
+		return NULL;
+	}
+	return NULL;
+}
