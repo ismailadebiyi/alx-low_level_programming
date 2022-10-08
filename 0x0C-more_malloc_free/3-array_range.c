669966b97f4425684c26040e3bd9e28c5e11a143
ismail adebiyi <ismailadebiyi@gmail.com>
2022-10-07 18:57:28 -0700

    add_3

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -14,14 +14,15 @@ int *array_range(int min, int max)
 	{
 		return (NULL);
 	}
-	a = malloc(sizeof(int) * (max - min + 1));
+	a = malloc(sizeof(int) * ((max - min) + 1));
 	if (a == NULL)
 	{
 		return (NULL);
 	}
-	for (i = 0; i < (max - min + 1); i++, min++)
+	for (i = 0; i < ((max - min) + 1); i++)
 	{
 		a[i] = min;
+		min++;
 	}
 	return (a);
 }
