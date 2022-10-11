c8d89736045ce16953896b6cd3f827ea29670b4f
ismail adebiyi <ismailadebiyi@gmail.com>
2022-10-11 15:03:58 -0700

    add_1-4

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,15 @@
+#include "dog.h"
+#include <stdlib.h>
+/**
+ * init_dog - initiallize dog 
+ * @d: dog Object
+ * @name: name of dog
+ * @owner: Dog owner
+ * @age: age of Dog
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->owner = owner;
+	d->age = age;
+	d->name = name;
+}
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * dog - Dog properties
+ * @name: name of dog
+ * @owner: Dog owner
+ * @age: age of Dog
+ */
+typedef struct dog
+{
+    /* data */
+    char *name;
+    char *owner;
+    float age;
+} dog_t;
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+#endif /*DOG_H*/
