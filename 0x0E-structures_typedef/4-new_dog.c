3578c009a1e72d022c07f8bbae4fcdb0b1bb68af
ismail adebiyi <ismailadebiyi@gmail.com>
2022-10-11 17:28:12 -0700

    add_4-5

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,7 +1,6 @@
 #include "dog.h"
 #include <stdio.h>
 #include <stdlib.h>
-
 /**
  * new_dog - create new dog
  * @name: name of dog
@@ -16,7 +15,8 @@ dog_t *new_dog(char *name, float age, char *owner)
 	d = malloc(sizeof(dog_t));
 	if (d == NULL || name == NULL || owner == NULL)
 	{
-
+		return (NULL);
+	}
 	d->name = name;
 	d->owner = owner;
 	d->age = age;
