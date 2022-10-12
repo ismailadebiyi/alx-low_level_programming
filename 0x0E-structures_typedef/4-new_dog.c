2ff9de91998523074320ab5ed9827412522aef59
ismail adebiyi <ismailadebiyi@gmail.com>
2022-10-11 17:14:34 -0700

    add_4-5

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,23 @@
+#include "dog.h"
+#include <stdio.h>
+/**
+ * new_dog - create new dog
+ * @name: name of dog
+ * @owner: Dog owner
+ * @age: age of Dog
+ * Return: new dog object
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *d;
+
+	d = malloc(sizeof(dog_t));
+	if (d == NULL || name == NULL || owner == NULL)
+	{
+		return (NULL);
+	}
+	d->name = name;
+	d->owner = owner;
+	d->age = age;
+	return (d);
+}
---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,15 @@
+
+#include <stdio.h>
+/**
+ * free_dog - free all dogs
+ * @d: pointer object
+ */
+void free_dog(dog_t *d)
+{
+	if (d)
+	{
+		free(d->owner);
+		free(d->name);
+		free (d);
+	}
+}
