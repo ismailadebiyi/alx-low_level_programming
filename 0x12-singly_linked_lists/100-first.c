4a416fe1e1602c5514e0676641ac58dbcc87e0c6
ismail adebiyi <ismailadebiyi@gmail.com>
2022-10-21 14:37:45 -0700

    add_100

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+void __attribute__((constructor)) premain(void);
+/**
+ * premain - print string before main
+ * function is executed
+ */
+
+void premain(void)
+{
+	printf("You're beat! and yet, you must allow,
+			\nI bore my house upon my back!\n");
+}
