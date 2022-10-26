ecd64b32389f82553e89626efc060d2ffe201c35
ismail adebiyi <ismailadebiyi@gmail.com>
2022-10-25 21:54:08 -0700

    add_5

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ * free_listint2 - free all memory allocated
+ * @head: head pointer
+*/
+void free_listint2(listint_t **head)
+{
+	listint_t *tmp;
+
+	while (*head)
+	{
+		tmp = *head->next;
+		free(*head);
+		*head = tmp;
+	}
+	head = NULL;
+}
