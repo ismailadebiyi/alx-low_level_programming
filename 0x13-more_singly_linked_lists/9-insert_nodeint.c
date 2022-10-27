e5fc6d2c3080b6c5b65ef86f87a3e16e70b696a0
ismail adebiyi <ismailadebiyi@gmail.com>
2022-10-26 19:28:49 -0700

    fix_8-9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -26,7 +26,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		*head = new;
 		return (new);
 	}
-	while (i != idx)
+	while (i != (idx - 1))
 	{
 		if (current == NULL || current->next == NULL)
 		{
